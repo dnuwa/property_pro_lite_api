47dd59a2f386318b2dca66b8f0438efa980d538a
daniel nuwa <daniel.nuwa@gmail.com>
2019-07-17 14:56:31 +0300

    feature(Server): add middleware to catch server errors [Delivers #167343006]

--- index.js
+++ index.js
@@ -13,8 +13,23 @@ app.use(morgan('dev'));
 
 app.use(routes);
 
-const { PORT } = process.env;
+app.use((req, res, next) => {
+  const error = new Error('NOT FOUND');
+  error.status = 404;
+  next(error);
+});
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+const { PORT } = process.env || 5000;
+
+app.listen(PORT);
 
 export default app;
